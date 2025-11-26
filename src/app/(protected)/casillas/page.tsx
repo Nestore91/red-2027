98b8f76cdc3856f587618d3e057313644c7a9013
Nestore91 <nestore9126@gmail.com>
2025-11-25 20:26:11 -0700

    Remove casillas completely (fix Vercel build error)

--- src/app/(protected)/casillas/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main className="p-6">
-      <h1 className="text-xl font-bold">Casillas</h1>
-      <p className="text-gray-600">Módulo en construcción…</p>
-    </main>
-  );
-}
